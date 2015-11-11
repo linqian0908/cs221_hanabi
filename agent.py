ad3978e0fe5b953728108f75caed880a741e0e7c
shanshanxu <xuss.icts@gmail.com>
2015-11-11 15:45:35 -0800

    maxmaxAgent, but still has bug

--- agent.py
+++ agent.py
@@ -21,3 +21,29 @@ class randomAgent(Agent):
         actions=gameState.getLegalActions(self.index)
         return random.sample(actions,1)[0]
         
+
+class MaxMaxAgent(Agent):
+    #similar to minimax, but here is a collaboration game:
+    def evaluationFunction(self, gameState):
+        return gameState.getScore()
+    def getAction(self, gameState):
+        optimalActions=[]
+        def recComputeVopt(gameState, depth, agentIndex):
+            Actions=gameState.getLegalActions(agentIndex)
+            if gameState.isEnd() or (not Actions):
+                return gameState.getScore()
+            if depth==0:
+                return self.evaluationFunction(gameState)
+            if agentIndex<gameState.getNumAgents()-1:
+                values=[recComputeVopt(gameState.generateSuccessor(agentIndex, a), depth, agentIndex+1) for a in Actions]
+            else:
+                values=[recComputeVopt(gameState.generateSuccessor(agentIndex, a), depth-1, 0) for a in Actions]
+            optimalValue=max(values)
+            if agentIndex==self.index and depth==self.depth:
+                for i, v in enumerate(values):
+                    if v==optimalValue:
+                        optimalActions.append(Actions[i])
+            return optimalValue
+        _=recComputeVopt(gameState, 1, self.index)
+        return random.choice(optimalActions)
+
